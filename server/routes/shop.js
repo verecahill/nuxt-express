f1af73efd693c5e33e73f4b0d7e326f2bd83000e
woojin <verecahill@gmail.com>
2019-08-09 16:07:35 +0900

    edited shop.js

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -1,1 +1,13 @@
-const express = require('express')
+const path = require("path");
+const express = require("express");
+const rootDir = require('../util/path');
+
+const adminData = require('./admin');
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+});
+
+module.exports = router;
