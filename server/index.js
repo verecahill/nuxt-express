4e8db36953bb7c85763a721a751253fafc6d34f0
woojin <verecahill@gmail.com>
2019-08-09 16:02:28 +0900

    added server files

--- server/index.js
+++ server/index.js
@@ -1,38 +1,53 @@
-const express = require('express')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
-const router = express.Router();
+const express = require("express");
+const consola = require("consola");
+const bodyParser = require("body-parser");
+const path = require("path");
+const rootDir = require("./util/path");
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const { Nuxt, Builder } = require("nuxt");
+const app = express();
+const app = express();
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
-config.dev = process.env.NODE_ENV !== 'production'
+const config = require("../nuxt.config.js");
+config.dev = process.env.NODE_ENV !== "production";
 
-async function start () {
+async function start() {
   // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+  const nuxt = new Nuxt(config);
 
-  const { host, port } = nuxt.options.server
+  const { host, port } = nuxt.options.server;
 
   // Build only in dev mode
   if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
+    const builder = new Builder(nuxt);
+    await builder.build();
   } else {
-    await nuxt.ready()
+    await nuxt.ready();
   }
-  app.use(express.json())
-  app.use(adminRoutes)
-  app.use(nuxt.render)
+  app.use(express.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(express.static("public"));
+  app.use("/admin", adminData.routes);
+  app.use(shopRoutes);
+  app.use((req, res, next) => {
+    res.status(404).render("404");
+  });
+  app.use(nuxt.render);
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port, host);
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
-  })
+  });
 }
-start()
+start();
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const router = express.Router();
+const express = require("express");
 
-router.get('/add-product',  (req, res, next) => {
-  res.send('add-product');
+const rootDir = require('../util/path');
+
+const router = express.Router();
+const path = require("path");
+
+const products = [];
+
+router.get("/add-product", (req, res, next) => {
+  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
-router.post('/product',  (req, res, next) => {
-  res.redirect('/');
-})
+// admin/add-product -> post
+router.post("/add-product", (req, res, next) => {
+  products.push({title: req.body.title});
+  res.redirect("/");
+});
 
-module.exports = router
+exports.routes = router;
+exports.products = products;
